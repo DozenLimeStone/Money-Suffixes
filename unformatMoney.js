fd27a79c17b0c6b51a3728cd5789ac0e2e2aeebf
Lime Stone <151057315+DozenLimeStone@users.noreply.github.com>
2025-09-21 12:44:52 +0200

    Update unformatMoney.js

--- unformatMoney.js
+++ unformatMoney.js
@@ -1,5 +1,5 @@
-//MADE BY DOZENLIMESTONE: https://github.com/DozenLimeStone/Money-Suffixes
-//YOU NEED TO INCLUDE THIS COMMENT IN YOUR CODE ©
+//MADE BY DOZENLIMESTONE ©: https://github.com/DozenLimeStone/Money-Suffixes
+//FEEL FREE TO MODIFY FUNCTIONS HOWEVER YOU LIKE
 function unformatMoney(stramount) {
     stramount=stramount.toLocaleString("fullwide",{useGrouping:false});
     if(isNaN(parseFloat(stramount))){return"Unvalid amount, source: unformatMoney. Can happen if someone puts a letter in front, or when a new bug appeared."; }
